2ec2b9e9f36e493031148f3aa2dcd1bcd0a41ccb
Gonzalo Alvarez Campos <40244960+Alvacampos@users.noreply.github.com>
2021-03-22 09:29:41 -0300

    Update yelp.js

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 export default axios.create({
   baseURL: "https://api.yelp.com/v3/businesses",
   headers: {
-    Authorization: 'Bearer vguwkxqP0-vwkHqBVtNsIsDE9qb_9D7X0PdH_rwTh25kaGatgL8zEhRxjkG8YnxUbQufNDDHHGkLdeMkFCdBt-fQVELIH6ibhqJR_1z6VJkA6FaLAnv3a6RpoOJRYHYx'
+    Authorization: 'Bearer '
   }
 })
